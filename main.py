ad2969354bf6aa2261f23e4e9cd65e4b506ef6fc
elk15 <84975908+elk15@users.noreply.github.com>
2023-05-03 09:48:07 +0300

    Create help method that prints instructions and asks for user input in terminal

--- main.py
+++ main.py
@@ -17,6 +17,39 @@ class Library:
         self.library_cur = self.library_con.cursor()
         self.existing_book_ids = [id[0] for id in self.library_cur.execute("SELECT bookid FROM books").fetchall()]
     
+    def help(self):
+        while True:
+            print("Welcome! Select one of the following options:")
+            print("1. Add new book")
+            print("2. Sort books by genre")
+            print("3. Exit")
+            choice = input()
+            if choice == '1':
+                while True:
+                    title = input("Enter the title (at least 2 characters): ")
+                    genre = input("Enter the genre (at least 2 characters): ")
+                    author = input("Enter the author (at least 2 characters): ")
+                    pages = input("Enter the pages (positive integers only): ")
+                    cover = input("Enter the link to the cover: ")
+                    published_date = input("Enter the publication date (YYYY-MM-DD format months and days are optional): ")
+                    author = input("Enter a short description (at least 2 characters): ")
+                    isbn = input("Enter the ISBN-13 barcode of the book: ")
+                    amount = input("Enter the amount of copies (positive integers only): ")
+                    success = self.add_new_book(title, genre, author, pages, cover, published_date, description, isbn, amount)
+                    if success:
+                        break
+            elif choice == '2':
+                genre = input("Please choose a genre : Fantasy, Romance, Science Fiction, Dystopian, Education, Mystery, Spirituality, Horror: ")
+                if len(self.sort_by_genre(genre)) == 0:
+                    print("No such genre exists in the database or incorrect input")
+                    continue
+                for book in self.sort_by_genre(genre):
+                    print(f"{book[1]} by {book[3]}")
+            elif choice == '3':
+                break
+            else:
+                print("Please enter an integer from 1-3")
+    
     def validate_book_input(self, title, genre, author, pages, cover, published_date, description, isbn, amount):
         """Checks if the input entered by the user when
         adding a new book is correct using regular expressions
@@ -34,7 +67,7 @@ class Library:
             print("Pages and amount must be a positive integer")
             return False
         if not re.fullmatch('^(?:[0-9]{4})(?:-(?:0[1-9]|1[0-2]))?(?:-(?:0[1-9]|1[0-9]|2[0-9]|3[0-1]))?$', str(published_date)):
-            print("Published date must be in this format YYYY-MM-DD month and day are optional")
+            print("Publication date must be in this format YYYY-MM-DD month and day are optional")
             return False
         if not re.fullmatch('^\S+\.\S+$', str(cover)):
             print("Cover must be a link")
@@ -82,9 +115,10 @@ class Library:
                 WHERE isbn = ?""", (amount, isbn))
                 self.library_con.commit()
                 print(f"{title} already exists in the database. Amount increased by {amount}")
+            return True
         else:
             print("Failed to add new book")
-            return
+            return False
 
     def sort_by_genre(self, genre):
         """Returns a list with books belonging in a specific category
@@ -93,12 +127,13 @@ class Library:
         return [book for book in self.library_cur.execute("SELECT * FROM books WHERE genre = ?", (genre,)).fetchall()]
 
     def main(self):
-       pass
+       self.help()
 
 
 if __name__ == "__main__":
     my_library = Library()
-    print(my_library.sort_by_genre('Fantasy'))
+    my_library.main()
+
 
 
     
