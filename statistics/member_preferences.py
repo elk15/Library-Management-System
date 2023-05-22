ae43d39cb7caca5929e1e00410f35aaef70f99ef
elk15 <84975908+elk15@users.noreply.github.com>
2023-05-22 11:06:35 +0300

    Create function that retrieves the dates where a member borrowed a book

---
+++ statistics/member_preferences.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+class MemberPreferences:
+    def __init__(self, member_id):
+        self.member_id = member_id
+        self.library_con = sqlite3.connect("library.db")
+        self.library_cur = self.library_con.cursor()
+        self.preferences = {}
+    
+    def get_dates(self):
+        return [date[0][3:] for date in self.library_cur.execute("SELECT date_borrowed FROM borrowed_books WHERE memberid = ?", (self.member_id,)).fetchall()]
+    
+    def get_books_per_month(self, date):
+        return [bookid for bookid in self.library_cur.execute("SELECT bookid FROM borrowed_books WHERE memberid = ?", (self.member_id,)).fetchall()]
+    
+    def get_preferences(self):
+        pass
+
+
+if __name__ == "__main__":
+    print(MemberPreferences(10).get_dates())
+    
+
+            
+
+    
