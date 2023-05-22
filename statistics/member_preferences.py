fe9dc5ff35e9114552bd1afcd24cc72d9c1fac92
elk15 <84975908+elk15@users.noreply.github.com>
2023-05-22 11:31:57 +0300

    Create function that finds a book's genre

--- statistics/member_preferences.py
+++ statistics/member_preferences.py
@@ -11,8 +11,11 @@ class MemberPreferences:
         return [date[0][3:] for date in self.library_cur.execute("SELECT date_borrowed FROM borrowed_books WHERE memberid = ?", (self.member_id,)).fetchall()]
     
     def get_books_per_month(self, date):
-        return [bookid for bookid in self.library_cur.execute("SELECT bookid FROM borrowed_books WHERE memberid = ? AND date_borrowed LIKE ?", (self.member_id, "___" + date,)).fetchall()]
+        return [bookid[0] for bookid in self.library_cur.execute("SELECT bookid FROM borrowed_books WHERE memberid = ? AND date_borrowed LIKE ?", (self.member_id, "___" + date,)).fetchall()]
     
+    def find_book_genre(self, book_id):
+        return [genre[0] for genre in self.library_cur.execute("SELECT genre FROM books WHERE bookid = ?", (book_id,)).fetchall()][0]
+
     def get_preferences(self):
         pass
 
@@ -20,6 +23,7 @@ class MemberPreferences:
 if __name__ == "__main__":
     print(MemberPreferences(10).get_dates())
     print(MemberPreferences(10).get_books_per_month("02/2023"))
+    print(MemberPreferences(10).find_book_genre('Bf7902450-f802-4218-8150-7d9284842299'))
     
 
             
