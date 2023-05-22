f44d0c9a83045bc8c2c84ebb59d6f0b48eac63a3
elk15 <84975908+elk15@users.noreply.github.com>
2023-05-22 11:15:59 +0300

    Create function that finds all books a member borrowed on a specific month

--- statistics/member_preferences.py
+++ statistics/member_preferences.py
@@ -11,7 +11,7 @@ class MemberPreferences:
         return [date[0][3:] for date in self.library_cur.execute("SELECT date_borrowed FROM borrowed_books WHERE memberid = ?", (self.member_id,)).fetchall()]
     
     def get_books_per_month(self, date):
-        return [bookid for bookid in self.library_cur.execute("SELECT bookid FROM borrowed_books WHERE memberid = ?", (self.member_id,)).fetchall()]
+        return [bookid for bookid in self.library_cur.execute("SELECT bookid FROM borrowed_books WHERE memberid = ? AND date_borrowed LIKE ?", (self.member_id, "___" + date,)).fetchall()]
     
     def get_preferences(self):
         pass
@@ -19,6 +19,7 @@ class MemberPreferences:
 
 if __name__ == "__main__":
     print(MemberPreferences(10).get_dates())
+    print(MemberPreferences(10).get_books_per_month("02/2023"))
     
 
             
